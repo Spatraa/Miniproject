098b5d436770cae79c46d5f2fa82020240d7e60d
sankar patra <sankarpatra573@gmail.com>
2022-07-08 10:35:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn=document.getElementById('btn1');
+btn.addEventListener("click",(()=>{
+    // alert("welcome to myntra.com")
+    location.href="https://www.myntra.com/"
+}))
+//  let navbar=document.getElementsByClassName('flex');
+//  navbar.addEventListener("click",(()=>{
+//     location.href="https://www.myntra.com/"
+//  }))
